7db06fe355558310e3b21708890bab4af03cb71f
williamKhine <williamKhine@users.noreply.github.com>
2022-05-24 09:59:25 +0630

    vault backup: 2022-05-24 09:59:24

--- Unit 1 - Programming Concepts and Skills/U1A6.py
+++ Unit 1 - Programming Concepts and Skills/U1A6.py
@@ -249,15 +249,15 @@ def insertionSort():
 
 # Selection Sort algorithm
 
-def selectionSort():
+# def selectionSort():
 
-    import time
-    randomList = makeRandomList()
-    l = len(randomList)
+#     import time
+#     randomList = makeRandomList()
+#     l = len(randomList)
 
-    lowestValIndex = 0
-    for i in range(1, l):
-        if randomList[i] < randomList[lowestValIndex]:
+#     lowestValIndex = 0
+#     for i in range(1, l):
+#         if randomList[i] < randomList[lowestValIndex]:
 
 
 
@@ -291,6 +291,7 @@ def factorial():
 # ================= #
 # insertDeleteIntegers()
 binarySearch()
+print(binarySearch.__doc__)
 linearSearch()
 # bubbleSort()
 # insertionSort()
--- Unit 1 - Programming Concepts and Skills/U1A9.py
+++ Unit 1 - Programming Concepts and Skills/U1A9.py
@@ -4,3 +4,8 @@ birth_place = input("Where were you born?: ")
 school = input("Which school did you attened?: ")
 favourite_subject = input("What is your favourite subject?: ")
 
+print(f"""
+To Whom This May Concern:
+My name is {first_name} {last_name}. I am originated from {birth_place}. I am currently attending
+{school} with my favorite course as {favourite_subject}.
+""")
